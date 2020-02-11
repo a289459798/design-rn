fe856fcb761e58f1d5a4b1d9cc72072c5f2f580b
zhangzy <zhangzy0816@gmail.com>
2020-02-11 17:37:52 +0800

    [代码完善]：headerview 增加线条

--- src/header/ICHeaderView.js
+++ src/header/ICHeaderView.js
@@ -4,6 +4,7 @@ import ICTouchableNativeFeedback from '../touchable/ICTouchableNativeFeedback';
 import ICFont from '../theme/ICFont';
 import ICText from '../text/ICText';
 import ICHeaderButton from './ICHeaderButton';
+import ICLine from '../view/ICLine';
 
 export default class ICHeaderView extends React.PureComponent {
 
@@ -13,38 +14,41 @@ export default class ICHeaderView extends React.PureComponent {
 
     render() {
         return (
-            <View
-                ref={e => this._view = e}
-                style={[{
-                    width: '100%',
-                    height: 44,
-                    backgroundColor: '#fff',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                    paddingHorizontal: ICFont.f10,
-                }, this.props.style]}>
-                {this.props.leftButtons ?
-                    <ICHeaderButton items={this.props.leftButtons} style={this.props.leftStyle}/>
-                    : this.props.leftComponent ? this.props.leftComponent : <View/>}
-                {this.props.centerComponent || this.props.title ?
-                    <View style={[{
-                        flex: 1,
+            <>
+                <View
+                    ref={e => this._view = e}
+                    style={[{
+                        width: '100%',
+                        height: 44,
+                        backgroundColor: '#fff',
+                        flexDirection: 'row',
                         alignItems: 'center',
-                        justifyContent: 'center',
-                        position: this.props.abs ? 'absolute' : '',
-                        left: 0,
-                        right: 0,
-                    }, this.props.centerStyle]}>
-                        {this.props.centerComponent ||
-                        <ICText style={{fontSize: ICFont.f16}}>
-                            {this.props.title}
-                        </ICText>}
-                    </View> : null}
-                {this.props.rightButtons ?
-                    <ICHeaderButton items={this.props.rightButtons} style={[this.props.rightStyle]}/>
-                    : this.props.rightComponent ? this.props.rightComponent : null}
-            </View>
+                        justifyContent: 'space-between',
+                        paddingHorizontal: ICFont.f10,
+                    }, this.props.style]}>
+                    {this.props.leftButtons ?
+                        <ICHeaderButton items={this.props.leftButtons} style={this.props.leftStyle}/>
+                        : this.props.leftComponent ? this.props.leftComponent : <View/>}
+                    {this.props.centerComponent || this.props.title ?
+                        <View style={[{
+                            flex: 1,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            position: this.props.abs ? 'absolute' : '',
+                            left: 0,
+                            right: 0,
+                        }, this.props.centerStyle]}>
+                            {this.props.centerComponent ||
+                            <ICText style={{fontSize: ICFont.f16}}>
+                                {this.props.title}
+                            </ICText>}
+                        </View> : null}
+                    {this.props.rightButtons ?
+                        <ICHeaderButton items={this.props.rightButtons} style={[this.props.rightStyle]}/>
+                        : this.props.rightComponent ? this.props.rightComponent : null}
+                </View>
+                {this.props.showLine ? <ICLine/> : null}
+            </>
         );
     }
 }
