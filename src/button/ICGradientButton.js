fea67fefa610e37b0b60ede7b7e907e5b589541c
zhangzy <zhangzy0816@gmail.com>
2020-03-27 14:38:01 +0800

    [代码完善](1.0.0): 优化header

--- src/button/ICGradientButton.js
+++ src/button/ICGradientButton.js
@@ -11,7 +11,7 @@ export default class ICGradientButton extends React.Component<ButtonProps, any> 
             {...this.props}
             ViewComponent={LinearGradient} // Don't forget this!
             linearGradientProps={{
-                colors: this.props.colors,
+                colors: this.props.disabled && this.props.disabledColor ? this.props.disabledColor : this.props.colors,
                 start: this.props.start || {x: 0, y: 0},
                 end: this.props.end || {x: 0, y: 1},
             }}
