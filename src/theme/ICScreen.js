700b6f017a965a4f43eb7a8b31e82d13b72a2e04
jszh <969883825@qq.com>
2020-11-17 13:24:23 +0800

    [Bug修复](1.0.0): iPhone12适配
    iPhone12适配

--- src/theme/ICScreen.js
+++ src/theme/ICScreen.js
@@ -3,6 +3,9 @@ import {Dimensions, Platform} from 'react-native';
 export default {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+    /**
+     * iPhoneX：刘海高度：44pt，底部高度：34pt
+     */
     iphonx: Platform.OS == 'ios' && (Dimensions.get('window').height >= 812),
     iphone12: Platform.OS == 'ios' && (Dimensions.get('window').height === 844 || Dimensions.get('window').height === 926),
     iphone: Platform.OS == 'ios',
